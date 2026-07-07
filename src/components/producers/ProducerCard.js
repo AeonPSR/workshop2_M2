7291bacea16825730e8d995e4210882c216a2ca8
Omargazzah02 <gazzahomar2001@gmail.com>
2026-07-07 16:52:50 +0200

    Feat: Add the producers page with data fetched from Odoo

---
+++ src/components/producers/ProducerCard.js
@@ -0,0 +1,32 @@
+
+export default function ProducerCard({ producer }) {
+
+  console.log("dsecription", producer.description)
+
+  return (
+    <div className="flex flex-col gap-4 items-center lg:w-96 w-80">
+      <div className="relative overflow-hidden rounded-lg aspect-4/5 w-full">
+        <img
+          src={producer.image}
+          alt="Brasserie du Vercors"
+          className="w-full h-full object-cover"
+        />
+
+        <div className="absolute inset-0 bg-linear-to-t from-black/80 via-black/40 to-transparent"></div>
+
+        <div className="absolute bottom-0 left-0 right-0 p-6">
+
+          <p className="text-xs uppercase tracking-wider text-gray-300 flex items-center gap-1">
+            {producer.address}
+          </p>
+
+          <h3 className="text-2xl font-bold text-white my-1">{producer.name}</h3>
+          <p className="text-xs uppercase font-semibold text-accent">Boissons</p>
+        </div>
+
+      </div>
+      <p className="lg:text-sm text-xs">{producer.description}</p>
+
+    </div>
+  )
+}
